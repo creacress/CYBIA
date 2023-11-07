3e2f7cf3ed67f142bbe55ed71824d49dc9405185
WebCresson <alexis@webcresson.com>
2023-11-07 19:07:02 +0100

    Sauvegarde + Optimisation

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,27 +1,29 @@
-import signal
+import logging
 import sys
-from transformers import CamembertTokenizer, CamembertForSequenceClassification, Trainer, TrainingArguments
-from datasets import Dataset
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_recall_fscore_support
-import pandas as pd
 import numpy as np
+import pandas as pd
+from datasets import Dataset
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from sklearn.model_selection import train_test_split
+from transformers import (CamembertForSequenceClassification, CamembertTokenizer,
+                          Trainer, TrainingArguments, TrainerCallback)
 import torch
 import gc
-import logging
 
 # Configurer le logging pour afficher les informations d'entraînement
 logging.basicConfig(level=logging.INFO)
 
 # Fonction pour gérer l'interruption et sauvegarder le modèle
 def save_on_interrupt(signal_received, frame):
-    print('Interruption reçue: Sauvegarde du modèle en cours...')
+    logging.info('Interruption reçue: Sauvegarde du modèle en cours...')
     trainer.save_model('toxicity_model_on_interrupt')
     sys.exit(0)
 
 # Configuration du gestionnaire de signaux pour intercepter Ctrl+C
+import signal
 signal.signal(signal.SIGINT, save_on_interrupt)
 
+# Fonction pour calculer les métriques de performance
 def compute_metrics(p):
     predictions, labels = p
     predictions = np.argmax(predictions, axis=1)
@@ -29,14 +31,14 @@ def compute_metrics(p):
     acc = accuracy_score(labels, predictions)
     return {'accuracy': acc, 'f1': f1, 'precision': precision, 'recall': recall}
 
-# Chargement des données rééquilibrées
-df = pd.read_csv('data/toxicity_french_upsampled.csv', sep=';')  # Utilisation du délimiteur ';'
-df = df[['Texte', 'oh_label']]  # Conserver uniquement les colonnes nécessaires
+# Charger les données
+df = pd.read_csv('data/toxicity_french_upsampled.csv', sep=';')
+df = df[['Texte', 'oh_label']]
 
-# Analyse rapide pour déterminer la longueur maximale utile
+# Déterminer la longueur maximale pour le tokenizer
 max_length = min(df['Texte'].str.len().quantile(0.95), 512)
 
-# Préparation des données pour l'entraînement
+# Split des données en train et validation sets
 train_df, val_df = train_test_split(df, stratify=df['oh_label'], test_size=0.1)
 
 # Initialisation du tokenizer
@@ -49,32 +51,31 @@ def tokenize_function(examples):
 # Création des datasets
 train_dataset = Dataset.from_pandas(train_df)
 val_dataset = Dataset.from_pandas(val_df)
-
-# Application de la tokenisation
 train_dataset = train_dataset.map(tokenize_function, batched=True)
 val_dataset = val_dataset.map(tokenize_function, batched=True)
-
-# Création des colonnes 'labels' pour les datasets
 train_dataset = train_dataset.rename_column("oh_label", "labels")
 val_dataset = val_dataset.rename_column("oh_label", "labels")
 
-# Configuration de l'entraînement
+# Configuration de l'entraînement avec le Trainer
 training_args = TrainingArguments(
     output_dir='./results',
-    evaluation_strategy="epoch",
+    evaluation_strategy="steps",
+    eval_steps=20,
     learning_rate=3e-5,
     per_device_train_batch_size=8,
     per_device_eval_batch_size=8,
     num_train_epochs=3,
     weight_decay=0.01,
     logging_dir='./logs',
-    save_strategy="epoch",
+    logging_steps=20,
+    save_strategy="steps",
+    save_steps=20,
     save_total_limit=2,
     load_best_model_at_end=True,
     metric_for_best_model="accuracy",
     greater_is_better=True,
     gradient_accumulation_steps=4,
-    use_cpu=True
+    use_cpu=False
 )
 
 # Initialisation du modèle
@@ -89,17 +90,24 @@ trainer = Trainer(
     compute_metrics=compute_metrics
 )
 
-# Entraînement dans un bloc try-except pour gérer l'interruption
+# Callback pour logging personnalisé
+class LoggingCallback(TrainerCallback):
+    def on_step_end(self, args, state, control, **kwargs):
+        if state.global_step % 20 == 0:
+            logging.info(f"Étape {state.global_step}: Entraînement en cours...")
+
+# Entraînement avec gestion des interruptions
 try:
-    trainer.train()
+    trainer.train(resume_from_checkpoint=True)
 except KeyboardInterrupt:
-    print("Interruption manuelle détectée pendant l'entraînement.")
+    logging.info("Interruption manuelle détectée pendant l'entraînement.")
 
 # Évaluation et sauvegarde du modèle
 eval_results = trainer.evaluate()
-print(eval_results)
+logging.info(eval_results)
 trainer.save_model('toxicity_model')
 
 # Nettoyage de la mémoire si nécessaire
 gc.collect()
-torch.cuda.empty_cache()
+if not training_args.use_cpu:
+    torch.cuda.empty_cache()
